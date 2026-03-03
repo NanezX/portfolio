d7402dec6d37daac42e475adf0d8e9e18daffbf8
NanezX <victor.nanezj@gmail.com>
2026-03-03 10:11:06 -0400

    feat: enhance ESLint configuration for Svelte support

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,24 @@
-import adapter from "@sveltejs/adapter-auto";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-import { mdsvex } from "mdsvex";
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess(), mdsvex()],
-  extensions: [".svelte", ".svx"],
+  extensions: ['.svelte', '.svx'],
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://svelte.dev/docs/kit/adapters for more information about adapters.
     adapter: adapter(),
+  },
+  // ESLint configuration for Svelte
+  eslint: {
+    parserOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      extraFileExtensions: ['.svelte'],
+    },
   },
 };
 
